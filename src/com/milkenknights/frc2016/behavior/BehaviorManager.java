4c5016cabe045c91c93224c5bddaf23831829d3d
Austin Shalit <austinshalit@gmail.com>
2016-01-27 23:05:59 -0800

    Remove robotSetpoints

--- src/com/milkenknights/frc2016/behavior/BehaviorManager.java
+++ src/com/milkenknights/frc2016/behavior/BehaviorManager.java
@@ -1,35 +1,23 @@
 package com.milkenknights.frc2016.behavior;
 
 import com.milkenknights.frc2016.HardwareAdapter;
-import com.milkenknights.frc2016.subsystems.Drive;
-import com.milkenknights.frc2016.subsystems.Intake;
-import com.milkenknights.frc2016.behavior.routines.ManualRoutine;
-import com.milkenknights.frc2016.behavior.routines.Routine;
 
 public class BehaviorManager {
     
-    private Routine currentRoutine;
-    private RobotSetpoints setpoints;
-    
-    private ManualRoutine manualRoutine = new ManualRoutine();
-    
     public BehaviorManager() {
-        setpoints = new RobotSetpoints();
-        setpoints.reset();
+
     }
     
+    /**
+     * Update the robot subsystems with the latest commands. 
+     * 
+     * @param commands The commands
+     */
     public void update(Commands commands) {
-        setpoints.reset();
         
-        setpoints = manualRoutine.update(commands, setpoints);
-        
-
-        if (setpoints.intakeAction == RobotSetpoints.IntakeAction.OPEN || setpoints.intakeAction == RobotSetpoints.IntakeAction.PREFER_OPEN) {
-
-        } else if (setpoints.intakeAction == RobotSetpoints.IntakeAction.CLOSE || setpoints.intakeAction == RobotSetpoints.IntakeAction.PREFER_CLOSE) {
-
+        if (commands.intakeSpeed != HardwareAdapter.INTAKE.getSpeed()) {
+            HardwareAdapter.INTAKE.setSpeed(commands.intakeSpeed);
         }
-        
         
     }
 }
