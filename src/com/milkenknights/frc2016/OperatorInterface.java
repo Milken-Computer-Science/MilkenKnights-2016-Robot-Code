ed73f769f542cadcc37e4abe34563996e7b03912
Austin Shalit <austinshalit@gmail.com>
2016-01-27 23:16:35 -0800

    Remove joystick references

--- src/com/milkenknights/frc2016/OperatorInterface.java
+++ src/com/milkenknights/frc2016/OperatorInterface.java
@@ -3,15 +3,9 @@ package com.milkenknights.frc2016;
 import com.milkenknights.frc2016.behavior.Commands;
 import com.milkenknights.frc2016.subsystems.Intake.IntakeSpeed;
 
-import edu.wpi.first.wpilibj.Joystick;
-
 public class OperatorInterface {
 
     private Commands commands = new Commands();
-    
-    Joystick leftStick = HardwareAdapter.LEFT_STICK;
-    Joystick rightStick = HardwareAdapter.RIGHT_STICK;
-    Joystick operatorStick = HardwareAdapter.OPERATOR_STICK;
     
     public void reset() {
         commands = new Commands();
@@ -21,7 +15,8 @@ public class OperatorInterface {
      * Return the commands that the operator wants to execute.
      */
     public Commands getCommands() {
-        if (operatorStick.getRawButton(1)) {
+
+        if (HardwareAdapter.OPERATOR_STICK.getRawButton(1)) {
             commands.intakeSpeed = IntakeSpeed.INTAKE;
         }
         
